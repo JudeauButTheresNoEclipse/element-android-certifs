2836885f13690d5c1f2a1271655683dcd6339fd3
Judeau BUTTHERESNOECLIPSE <emanone@tutanota.com>
2023-06-01 10:41:14 +0200

    Remove sleep

--- matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/network/httpclient/OkHttpClientUtil.kt
+++ matrix-sdk-android/src/main/java/org/matrix/android/sdk/internal/network/httpclient/OkHttpClientUtil.kt
@@ -42,7 +42,6 @@ internal fun OkHttpClient.Builder.addAccessTokenInterceptor(accessTokenProvider:
 
 internal fun OkHttpClient.Builder.addSocketFactory(homeServerConnectionConfig: HomeServerConnectionConfig): OkHttpClient.Builder {
     try {
-        Thread.sleep(3000)
         val pair = CertUtil.newPinnedSSLSocketFactory(homeServerConnectionConfig)
         sslSocketFactory(pair.sslSocketFactory, pair.x509TrustManager)
         hostnameVerifier(CertUtil.newHostnameVerifier(homeServerConnectionConfig))
